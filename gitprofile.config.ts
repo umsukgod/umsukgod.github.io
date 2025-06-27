d63a60dc3f1f4184f98ce62ffee35042c7ae17ce
minseok <umsukgod@hanmail.net>
2025-06-27 21:12:09 +0900

    filling in

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -10,52 +10,52 @@ const CONFIG = {
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
   base: '/',
-  // projects: {
-  //   github: {
-  //     display: true, // Display GitHub projects?
-  //     header: 'Github Projects',
-  //     mode: 'automatic', // Mode can be: 'automatic' or 'manual'
-  //     automatic: {
-  //       sortBy: 'stars', // Sort projects by 'stars' or 'updated'
-  //       limit: 8, // How many projects to display.
-  //       exclude: {
-  //         forks: false, // Forked projects will not be displayed if set to true.
-  //         projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
-  //       },
-  //     },
-  //     manual: {
-  //       // Properties for manually specifying projects
-  //       projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
-  //     },
-  //   },
-  //   // external: {
-  //   //   header: 'My Projects',
-  //   //   // To hide the `External Projects` section, keep it empty.
-  //   //   projects: [
-  //   //     {
-  //   //       title: 'Project Name',
-  //   //       description:
-  //   //         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-  //   //       imageUrl:
-  //   //         'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-  //   //       link: 'https://example.com',
-  //   //     },
-  //   //     {
-  //   //       title: 'Project Name',
-  //   //       description:
-  //   //         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-  //   //       imageUrl:
-  //   //         'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-  //   //       link: 'https://example.com',
-  //   //     },
-  //   //   ],
-  //   // },
-  // },
-  seo: {
-    title: 'Portfolio of Ariful Alam',
-    description: '',
-    imageURL: '',
+  projects: {
+    github: {
+      display: false, // Display GitHub projects?
+      header: 'Github Projects',
+      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      automatic: {
+        sortBy: 'stars', // Sort projects by 'stars' or 'updated'
+        limit: 8, // How many projects to display.
+        exclude: {
+          forks: false, // Forked projects will not be displayed if set to true.
+          projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        },
+      },
+      manual: {
+        // Properties for manually specifying projects
+        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+      },
+    },
+    // external: {
+    //   header: 'My Projects',
+    //   // To hide the `External Projects` section, keep it empty.
+    //   projects: [
+    //     {
+    //       title: 'Project Name',
+    //       description:
+    //         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+    //       imageUrl:
+    //         'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+    //       link: 'https://example.com',
+    //     },
+    //     {
+    //       title: 'Project Name',
+    //       description:
+    //         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+    //       imageUrl:
+    //         'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+    //       link: 'https://example.com',
+    //     },
+    //   ],
+    // },
   },
+  // seo: {
+  //   title: 'Portfolio of Ariful Alam',
+  //   description: '',
+  //   imageURL: '',
+  // },
   social: {
     // linkedin: 'ariful-alam',
     // x: 'arif_szn',
@@ -80,7 +80,8 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+    '', // Empty fileUrl will hide the `Download Resume` button.
+    // 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Python',
